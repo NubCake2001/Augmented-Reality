476ff7e8ddddc50000e89d0415be577bf514ffdd
NubCake2001 <nubcake2001@gmail.com>
2021-05-13 15:02:56 +0530

    Aspect changed

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threej
 
 function main() {
   const canvas = document.querySelector('#c');
+  canvas.width = 700;
+  canvas.height = 700;
   const renderer = new THREE.WebGLRenderer({ canvas });
 
   const fov = 75;
-  const aspect = 2;  // the canvas default
+  const aspect = 1;  // the canvas default
   const near = 0.1;
   const far = 5;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
@@ -13,9 +15,9 @@ function main() {
 
   const scene = new THREE.Scene();
 
-  const boxWidth = 2;
-  const boxHeight = 2;
-  const boxDepth = 2;
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
   const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });  // greenish blue
