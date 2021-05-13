229da77f908753e38de1e419c96742a1916e8958
NubCake2001 <nubcake2001@gmail.com>
2021-05-13 14:31:52 +0530

    cube chamge

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threej
 
 function main() {
   const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer({canvas});
+  const renderer = new THREE.WebGLRenderer({ canvas });
 
   const fov = 75;
   const aspect = 2;  // the canvas default
@@ -13,12 +13,12 @@ function main() {
 
   const scene = new THREE.Scene();
 
-  const boxWidth = 1;
-  const boxHeight = 1;
-  const boxDepth = 1;
+  const boxWidth = 2;
+  const boxHeight = 2;
+  const boxDepth = 2;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });  // greenish blue
 
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
